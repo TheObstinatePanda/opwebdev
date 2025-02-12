d84f6575e3122b141744c3bee68f850bbd8ff320
OPWebDev <j.m.newman.101@gmail.com>
2025-02-12 17:12:28 -0500

    updating CORS to allow frontend to make requests to the backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://www.opwebdev.com',
+    methods: 'POST',
+    allowedHeaders: 'Content-Type',
+}));
 
 console.log('Api key: ',process.env.SENDGRID_API_KEY)
 
