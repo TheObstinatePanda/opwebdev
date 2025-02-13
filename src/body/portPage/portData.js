c90f04e6a21d1e3c85d1dbbb856796ec5d195158
OPWebDev <j.m.newman.101@gmail.com>
2025-02-13 17:10:16 -0500

    Updating CSS for Services and Portfolio pages to make it more dynamic

--- src/body/portPage/portData.js
+++ src/body/portPage/portData.js
@@ -9,6 +9,7 @@ const elements = [
         text: "A website created for a local non-profit organization dedicated to providing affordable spay and neuter services for pets in the community.",
         link: "https://paradoxspayneuter.netlify.app/",
     },
+    
    
 ]
 
